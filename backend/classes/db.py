6c7f8e95648b26a6dffed0d696ec8d0e07f87a4c
Rychard <87041036+Rychard355@users.noreply.github.com>
2022-05-16 11:11:21 -0400

    db register increment

--- backend/classes/db.py
+++ backend/classes/db.py
@@ -8,9 +8,19 @@ class db:
         __password = ""
 
         self.__db = mysql.connector.connect(__host, __database, __user, __password)
+        self.__dbCursor = self.__db.cursor()
 
-        if (self.__db.is_connect()):
+        if self.__db.is_connect():
             print("erro") # erros deveram ser tratados pela futura classe "error"
     
-    def get(self, table, where): # tem como intuito pegar alguma informação do banco de dados
-        pass
+    def register(self, surname, name, email, password):
+        sql = "INSERT INTO users (`surname`, `name`, `email`, `password`) values (%s, %s, %s, %s)"
+        val = (surname, name, email, password)
+
+        self.__dbCursor.execute(sql, val)
+
+    def get(self, column, table, value): # VERIFCAR O RETORNO DESTA FUNÇÃO
+        sql = "SELECT %s FROM %s WHERE %s = %s"
+        val = (column, table, column, value)
+
+        return self.__dbCursor.execute(sql, val)
--- backend/classes/user.py
+++ backend/classes/user.py
@@ -1,3 +1,4 @@
+from contextlib import nullcontext
 import db
 
 class user:
@@ -5,5 +6,13 @@ class user:
         self.__db = db.DB()
 
     def register(self, surname, name, email, password): # registra um novo usuário
-        pass
+        if self.__db.get('email', 'users', email) or self.__db.get('surname', 'users', surname): #----VEFIQUE MELHOR O RETORNO DA FUNÇÃO GET
+            return False
+
+        self.__db.register(surname, name, email, password)
+        return True
     
+    def login(self, surname, password):
+        if self.__db.get('surname', 'users', surname) or self.__db.get('password', 'users', password):
+            return False
+        return True
